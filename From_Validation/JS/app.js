edfa29201aa089fe28485b9fef086f40449c25c5
cr7st74n <34308684+cr7st74n@users.noreply.github.com>
2023-02-09 15:05:04 -0500

    getting all validation readdy

--- From_Validation/JS/app.js
+++ From_Validation/JS/app.js
@@ -13,13 +13,21 @@
 
     let orderSummaryParagraph = document.querySelector('#order-summary')
 
-    
+    function ClassErrors(val){
+        val.classList.add("error")
+    }
+
+    function DeleteError(val){
+        val.classList.remove("error")
+    }
+
     submitButton.addEventListener('click', function() {
 
         let name = homeownerNameInput.value 
         let houseNumber = homeownerHouseNumberInput.value
         let streetName = homeownerStreetNameInput.value
         let treeType = treeTypeSelect.value
+
 
         let i = 0;
 
@@ -31,21 +39,25 @@
         // TODO Validate that all six input and select elements have been completed
         //  - check they all have a value
         const InpValues = [name, houseNumber, streetName, treeType, city,zipcode];
-        const ErrorsEmpty = [];
+        const Errors = [];
 
         InpValues.forEach( (valueInp)=>{
             i++;
             if (valueInp === ""){
-                ErrorsEmpty.push(`The input Number ${i} is empty! Correct it please.`);
+                Errors.push(`The input Number ${i} is empty! Correct it please.`);
             }
             console.log(valueInp);
         })
-        if (ErrorsEmpty.length > 0 ){
-            alert(ErrorsEmpty.join("\n"));
-            return;
-        }
+
         
         // TODO Validate the zipcode is in the range 55001 and 56763. 
+
+        if(zipcode < 55001 || zipcode >56763){
+            Errors.push(`The zipcode is different from the State of Minnesota`);
+            ClassErrors(zipcodeInput);
+        }else{
+            DeleteError(zipcodeInput)
+        }
         // to check that the zip code is in Minnesota.
         // Make sure your script doesn't accept non-numeric input for the zip code. 
 
@@ -54,23 +66,45 @@
         // There's a style for input.error and select.error defined in the styles (in the head)
         // which will be applied if you give the class .error to any elements on the page. 
         // Make sure you remove this class when the form is completed correctly.
-
-        let errors = []
         
         // If a form element has not been completed, or if the data is not valid, 
         // add an error message to the errors array. (Hint: use push to add to the end of the array)
         // Add one error message for each problem with the form. 
 
-        
+        // if(!name){
+        //     Errors.push("Please correct your name")
+        //     ClassErrors(homeownerNameInput);
+        // }
+
+        ValidateData(homeownerNameInput);
+        ValidateData(homeownerHouseNumberInput);
+        ValidateData(homeownerStreetNameInput);
+        ValidateData(treeTypeSelect);
+
+        function ValidateData(data){
+            if(!data.value){
+                Errors.push(`Please correct your ${data.name}`)
+                ClassErrors(data);
+            }else{
+                DeleteError(data);
+            }
+        }
+
         // TODO - If there are any errors after checking all of the input elements, 
         // display an alert with all of the error messages. (Hint: use join)
         // and then return from this function to prevent further processing 
 
+        if (Errors.length > 0 ){
+            alert(Errors.join("\n"));
+            return;
+        }
 
         // TODO - If there are no errors, use a template string to display an order 
         // summary in the order summary paragraph.
 
         
     })
+
+
 
 
