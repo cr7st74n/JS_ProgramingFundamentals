e516dc22563185b3e7ea551b3a5524cbef707f57
cr7st74n <34308684+cr7st74n@users.noreply.github.com>
2023-02-09 23:26:28 -0500

    printing data form a JSON File

--- ArraysAndObjects/ArrAndObj.js
+++ ArraysAndObjects/ArrAndObj.js
@@ -18,10 +18,13 @@ let iss_location = {
 }
 
 // TODO Extract the latitude value, and log it to the console.
+console.log('1) TODO Extract the latitude value');
+const latitude = iss_location.iss_position.latitude;
+console.log(latitude);
 // TODO Extract the longitude value, and log it to the console.
-
-
-
+console.log('2) TODO Extract the longitude value');
+const longitude = iss_location.iss_position.longitude;
+console.log(longitude);
 
 /* b. Use this JavaScript object of exchange rates relative to Euros.
 The properties are currency symbols, the values are the exchange rates.
@@ -37,13 +40,21 @@ let rates = {
 }
 
 // TODO write code to add a new property for Swiss Francs. Symbol is CHF, value is 1.1787.
+rates.CHF = 1.1787;
+console.log(rates);
 // TODO if you had 100 Euros, write code to get the exchange rate from the object, then calculate 
 //      the equivalent value in Australian Dollars (AUD)
+const AD = rates.AUD;
+const convertion = AD*100
+console.log(convertion.toFixed(2));
 // TODO write code to identify the currency symbol that has the highest exchange rate compared to Euros.
 //    In other words, identify the property with the largest value. the answer is BRL (Brazilian Real) at 3.8959 BRL to 1 Euro.
+const BG = rates.BGN, BR = rates.BRL, CA = rates.CAD, CH = rates.CHF, EU = 1;
 
+const Data = [BG,BR,CA,CH,EU];
 
-
+HighestExc = Math.max.apply(null,Data);
+console.log(HighestExc);
 
 /* c. Use this JavaScript array of objects of cat owners, and their cats. Source, moderncat.com
  */
@@ -56,22 +67,23 @@ let cats_and_owners = [
 ]
 
 // TODO print Gary Oldman's cat's name
+console.log('\n c. Use this JavaScript array of objects of cat owners, and their cats.');
+const GO_Cat = cats_and_owners[1].cat
+console.log(`The  Gary Oldman's cat's name is ${GO_Cat}`);
 // TODO Taylor Swift's cat is called 'Meredith'. Write code to add this data to the array.
+cats_and_owners.push({name:"Taylor Swift", cat: "Meredith"})
+console.log(cats_and_owners);
 // TODO write a loop to print each cat owner, and their cat's name, one per line. Use the forEach style.
 //   Each line should have a message like "Snoop Dogg's cat is called Miles Davis"
+cats_and_owners.forEach((CatOwner)=>{
+    console.log(`${CatOwner.name}'s cat is called ${CatOwner.cat}`);
+})
 
 
 
 /* d. Use the following JSON object, describing the Nobel Prize winners in 2017.
 Source http://api.nobelprize.org/v1/prize.json?year=2017
 * */
-
-// TODO print the full name of the Literature Nobel laureate.
-// TODO print the ids of each of the Physics Nobel laureates. Your code should still work without modification if a laureate was added, or removed.
-// TODO write code to print the names of all of the prize categories (So your output would start physics, chemistry, medicine... ).
-// TODO write code to print the total number of prize categories
-// TODO write code to count the total number of laureates from 2017. 
-//   have a good look at how the JSON is structured, and think about what loop(s) you'll need to write.
 
 
 let nobel_prize_winners_2017 = {
@@ -198,3 +210,13 @@ let nobel_prize_winners_2017 = {
     }
   ]
 }
+
+// TODO print the full name of the Literature Nobel laureate.
+console.log('\n TODO Novel Prizes \n Literature Nobel');
+const LitWinnerName = nobel_prize_winners_2017.prizes[3].laureates[0].firstname +" " +nobel_prize_winners_2017.prizes[3].laureates[0].surname;
+console.log(LitWinnerName);     
+// TODO print the ids of each of the Physics Nobel laureates. Your code should still work without modification if a laureate was added, or removed.
+// TODO write code to print the names of all of the prize categories (So your output would start physics, chemistry, medicine... ).
+// TODO write code to print the total number of prize categories
+// TODO write code to count the total number of laureates from 2017. 
+//   have a good look at how the JSON is structured, and think about what loop(s) you'll need to write.
